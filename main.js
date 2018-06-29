fa8157ebdb69898b026344cc00e445c42485a285
antyoku <antyokunaoyasu@gmail.com>
2018-06-29 14:28:28 +0900

    fruits expo

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
-var app = new ({
-  el: '#app'
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Fruits Expo!'
+  }
+})
+
+var list = new Vue({
+  el: '#list',
+  data: {
+    fruits: ['apple', 'banana', 'strawberry']
+  }
+})
+
+var count = 0
+
+var click = new Vue({
+  el: '#click',
+  methods: {
+    handleClick: function(event) {
+      list.fruits.push('unko')
+      count++
+      if (count > 3) {
+        attention.show = true
+      }
+    }
+  }
+})
+
+var attention = new Vue({
+  el: '#attention',
+  data: {
+    show: false
+  }
 })
