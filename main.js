ac4f1bc2230e9444cd95eef3dafba5b3687f254a
antyoku <antyokunaoyasu@gmail.com>
2018-08-12 19:34:00 +0900

    reset

--- main.js
+++ main.js
@@ -1,35 +1,10 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Fruits Expo!'
-  }
-})
-
-var list = new Vue({
-  el: '#list',
-  data: {
+    message: {
+      value: 'Fruits Expo!'
+    },
+    show: true,
     fruits: ['apple', 'banana', 'strawberry']
-  }
-})
-
-var count = 0
-
-var click = new Vue({
-  el: '#click',
-  methods: {
-    handleClick: function(event) {
-      list.fruits.push('unko')
-      count++
-      if (count > 3) {
-        attention.show = true
-      }
-    }
-  }
-})
-
-var attention = new Vue({
-  el: '#attention',
-  data: {
-    show: false
   }
 })
